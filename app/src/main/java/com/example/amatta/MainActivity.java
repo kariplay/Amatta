271d783af4d62c0e6a50146c3dfd4b384b283d29
kariplay <37902885+kariplay@users.noreply.github.com>
2023-12-05 19:19:43 +0900

    spinner 코드 추가
    spinner 코드 추가

--- app/src/main/java/com/example/amatta/MainActivity.java
+++ app/src/main/java/com/example/amatta/MainActivity.java
@@ -1,4 +1,79 @@
 package com.example.amatta;
 
-public class MainActivity {
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import androidx.core.app.ActivityCompat;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity  {
+    private Spinner first;
+    ArrayAdapter<CharSequence> adspin1, adspin2; //어댑터를 선언 adspint1(서울,부산..) adspin2(강남구,강서구..)
+    Button btpaired, btsend;
+    ListView btlist;
+
+    BluetoothAdapter btAdapter;
+    private final static int REQUEST_ENABLE_BT = 1;
+
+    Set<BluetoothDevice> pairedDevice;
+    ArrayAdapter<String> btArrayAdapter;
+    ArrayList<String> deviceAddressArray;
+    BluetoothSocket btSocket = null;
+    ConnectedThread connectedThread;
+
+    String TAG = "MainActivity";
+    UUID BT_MODULE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    int x, y;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btpaired = (Button) findViewById(R.id.BTpaired);
+        btsend = (Button) findViewById(R.id.BTsend);
+
+        btlist = (ListView) findViewById(R.id.BTlist);
+
+        btlist.setOnItemClickListener(new myOnItemClickListener());
+
+        final Spinner spin1 = (Spinner) findViewById(R.id.first);
+        final Spinner spin2 = (Spinner) findViewById(R.id.second);
+
+        adspin1 = ArrayAdapter.createFromResource(this, R.array.first_region, android.R.layout.simple_spinner_dropdown_item);
+        adspin1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin1.setAdapter(adspin1);
+
+        btArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        deviceAddressArray = new ArrayList<>();
+        btlist.setAdapter(btArrayAdapter);
+
+        String[] permission_list = {
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.BLUETOOTH_SCAN,
+                android.Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.BLUETOOTH_CONNECT
+        };
+
+        ActivityCompat.requestPermissions(MainActivity.this, permission_list, 1);
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!btAdapter.isEnabled()) {
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT);
+        }{
 }

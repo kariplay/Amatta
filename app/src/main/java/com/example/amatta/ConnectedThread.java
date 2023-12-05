5f414f2de9a4ba323fe1ed2e6a6a8f7cd0649167
kariplay <37902885+kariplay@users.noreply.github.com>
2023-12-05 19:24:10 +0900

    커넥트 관련 코드 추가 및 수정
    커넥트 관련 코드 추가 및 수정

--- app/src/main/java/com/example/amatta/ConnectedThread.java
+++ app/src/main/java/com/example/amatta/ConnectedThread.java
@@ -52,3 +52,21 @@ public class ConnectedThread extends Thread {
             }
         }
 
+    /* Call this from the main activity to send data to the remote device */
+    public void write(String input) {
+        byte[] bytes = input.getBytes();           //converts entered String into bytes
+        try {
+            mmOutStream.write(bytes);
+        } catch (IOException e) {
+        }
+    }
+
+    /* Call this from the main activity to shutdown the connection */
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) {
+        }
+    }
+}
+
